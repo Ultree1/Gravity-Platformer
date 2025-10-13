34f206cc4f1029c2209ef648d5cfd4c58d3dd6a3
OrobosaA <orobosaaghahowa1@gmail.com>
2025-10-13 00:51:42 -0700

    Initial commit – Unity project setup

---
+++ Assets/Scripts/MonsterStomp.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class MonsterStomp : MonoBehaviour
+{
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Weak Point")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
