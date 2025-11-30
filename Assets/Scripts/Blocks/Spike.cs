97c25fb6759be9b25e78e8823f693ff67a5eb084
dmonde13 <damonm812@gmail.com>
2025-11-30 13:49:20 -0800

    Spike hurt SFX
    Added hurt/damage sfx support to spike before game over

--- Assets/Scripts/Blocks/Spike.cs
+++ Assets/Scripts/Blocks/Spike.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
+using System.Collections;
 
 public class Spike : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip damageClip;
+
     private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.CompareTag("Player")) {
-			GameManager.Instance.GameOver();
+            StartCoroutine(HandlePlayerHit());
 		}
         else if (other.gameObject.layer == LayerMask.NameToLayer("Block"))
 		{
 			Destroy(other.gameObject);
 		}
 	}
+
+    private IEnumerator HandlePlayerHit()
+    {
+        if (audioSource != null && damageClip != null)
+        {
+            audioSource.volume = 1f;
+            audioSource.PlayOneShot(damageClip);
+            yield return new WaitForSeconds(0.3f);
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.3f);
+        }
+
+        GameManager.Instance.GameOver();
+    }
 }
